fa9c9028c740673fa3ad494defbd367b9c04be4c
ridoybabu781 <itzking.rk@gmail.com>
2025-08-12 19:09:15 +0600

    updated review routes

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { addReview, removeReview } = require("../controllers/review.controller");
+const {
+  addReview,
+  removeReview,
+  editReview,
+  myReviews,
+  productReviews,
+} = require("../controllers/review.controller");
 const User = require("../middlewares/User");
 const upload = require("../utils/multer");
 
@@ -7,5 +13,8 @@ const reviewRouter = express.Router();
 
 reviewRouter.post("/addReview/:id", User, upload.array("images", 5), addReview);
 reviewRouter.post("/removeReview", User, removeReview);
+reviewRouter.post("/editReview", User, upload.array("images", 5), editReview);
+reviewRouter.post("/myReviews", User, myReviews);
+reviewRouter.post("/productReviews", productReviews);
 
 module.exports = reviewRouter;
