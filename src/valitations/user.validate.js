71111341c54fc03b060de9a05768fa102f7c7d56
ridoybabu781 <itzking.rk@gmail.com>
2025-08-15 17:09:14 +0600

    verification code section editeed

--- src/valitations/user.validate.js
+++ src/valitations/user.validate.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const registerValidation = Joi.object({
   name: Joi.string().min(3).required(),
   email: Joi.string().email().required(),
+  verificationCode: Joi.number().required(),
   password: Joi.string().min(6).required(),
   role: Joi.string().valid("user", "admin").default("user"),
   profilePic: Joi.string().uri().optional(),
