53a040e7d9225e8f8a00651399076ea27d24ae6c
ridoybabu781 <itzking.rk@gmail.com>
2025-08-15 18:25:41 +0600

    message Updated

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -2,7 +2,9 @@ const validate = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body, { abortEarly: false });
   if (error) {
     const messages = error.details.map((detail) => detail.message);
-    return res.status(400).json({ success: false, errors: messages });
+    return res
+      .status(400)
+      .json({ success: false, errors: messages, message: messages });
   }
   next();
 };
