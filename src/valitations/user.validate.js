58891a171d1693b33d580d15c3298c7a56508c3a
ridoybabu781 <itzking.rk@gmail.com>
2025-08-15 17:12:43 +0600

    registration validation updated

--- src/valitations/user.validate.js
+++ src/valitations/user.validate.js
@@ -3,16 +3,9 @@ const Joi = require("joi");
 const registerValidation = Joi.object({
   name: Joi.string().min(3).required(),
   email: Joi.string().email().required(),
-  verificationCode: Joi.number().required(),
+  verificationCode: Joi.number().required().min(6),
   password: Joi.string().min(6).required(),
   role: Joi.string().valid("user", "admin").default("user"),
-  profilePic: Joi.string().uri().optional(),
-
-  address: Joi.string().optional(),
-  age: Joi.number().optional(),
-  birthDate: Joi.date().optional(),
-  gender: Joi.string().optional(),
-  phone: Joi.number().optional(),
 });
 
 const updateProfileValidation = Joi.object({
@@ -31,6 +24,11 @@ const updateProfilePicValidation = Joi.object({
   size: Joi.number()
     .max(5 * 1024 * 1024)
     .required(),
+});
+
+const loginValidation = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
 });
 
 module.exports = {
