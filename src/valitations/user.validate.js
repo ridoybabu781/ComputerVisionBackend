de71f9eaf8afd6685b357c7839aab362c95c4acf
ridoybabu781 <itzking.rk@gmail.com>
2025-08-15 14:21:33 +0600

    validation changed

--- src/valitations/user.validate.js
+++ src/valitations/user.validate.js
@@ -4,21 +4,29 @@ const registerValidation = Joi.object({
   name: Joi.string().min(3).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
-  verificationCode: Joi.string().optional(),
+  role: Joi.string().valid("user", "admin").default("user"),
+  profilePic: Joi.string().uri().optional(),
+
+  address: Joi.string().optional(),
+  age: Joi.number().optional(),
+  birthDate: Joi.date().optional(),
+  gender: Joi.string().optional(),
+  phone: Joi.number().optional(),
 });
 
 const updateProfileValidation = Joi.object({
-  name: Joi.string().min(3).required(),
+  name: Joi.string().min(3).optional(),
   birthDate: Joi.date().optional(),
   age: Joi.number().optional(),
   gender: Joi.string().optional(),
   address: Joi.string().optional(),
-  phone: Joi.string().optional(),
+  phone: Joi.number().optional(),
+  profilePic: Joi.string().uri().optional(),
 });
 
 const updateProfilePicValidation = Joi.object({
   originalname: Joi.string().required(),
-  mimetype: Joi.string().valid("image/jpeg", "image/png").required(),
+  mimetype: Joi.string().required(),
   size: Joi.number()
     .max(5 * 1024 * 1024)
     .required(),
